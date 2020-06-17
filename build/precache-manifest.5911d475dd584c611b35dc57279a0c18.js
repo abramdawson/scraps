3b52d000b8abb2e4c0329e988cde70c2c1a9d1e4
abramdawson <abramdawson@gmail.com>
2020-06-17 16:54:38 -0700

    build

---
+++ build/precache-manifest.5911d475dd584c611b35dc57279a0c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4835d5d095b7118ec7b30088f704f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "314e53affb777b302c58",
+    "url": "/static/css/main.e557140e.chunk.css"
+  },
+  {
+    "revision": "7abf36ea20fed7f535f9",
+    "url": "/static/js/2.0a8cb8ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a8cb8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314e53affb777b302c58",
+    "url": "/static/js/main.6b597c06.chunk.js"
+  },
+  {
+    "revision": "82256fe2f3d67f0ff635",
+    "url": "/static/js/runtime-main.cc92aa8d.js"
+  }
+]);
